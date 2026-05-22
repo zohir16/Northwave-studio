845482ee78ca11dfe07330f017d560d4e3e37b71
zohir <coderpro57gmail.com>
2026-05-22 16:17:02 -0700

    intial website launch

---
+++ js/main.js
@@ -0,0 +1,98 @@
+const toggle = document.querySelector(".nav__toggle");
+const menu = document.querySelector(".nav__menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+console.log("JS is working");
+
+const links = document.querySelectorAll(".nav__menu a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+
+    // active link styling
+    links.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+
+    // CLOSE mobile menu
+    menu.classList.remove("active");
+  });
+});
+
+/* =========================
+   WORK SLIDER
+========================= */
+
+const slides = document.querySelectorAll(".work__slide");
+
+const dots = document.querySelectorAll(".work__dot");
+
+dots.forEach((dot, index) => {
+
+  dot.addEventListener("click", () => {
+
+    slides.forEach(slide => {
+      slide.classList.remove("active");
+    });
+
+    dots.forEach(dot => {
+      dot.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+
+    dots[index].classList.add("active");
+  });
+
+});
+
+/* AUTO SLIDER */
+
+let currentSlide = 0;
+
+setInterval(() => {
+
+  slides.forEach(slide => {
+    slide.classList.remove("active");
+  });
+
+  dots.forEach(dot => {
+    dot.classList.remove("active");
+  });
+
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  slides[currentSlide].classList.add("active");
+
+  dots[currentSlide].classList.add("active");
+
+}, 4000);
+
+
+/* orelay */
+
+const toggleBtn = document.querySelector(".nav__toggle");
+const closeBtn = document.querySelector(".nav__close");
+const navMenu = document.querySelector(".nav__menu");
+const overlay = document.querySelector(".nav__overlay");
+
+toggleBtn.addEventListener("click", () => {
+  navMenu.classList.add("active");
+  overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
